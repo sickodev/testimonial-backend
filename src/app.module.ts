37e3dc71cda74fe35eccff03dd0541b9f376b0ff
Kumar <kbishwa03kalyan@gmail.com>
2024-10-17 00:27:58 +0530

    added prisma and user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { OrgModule } from './org/org.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HealthModule, OrgModule],
+  imports: [HealthModule, OrgModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/org/org.module.ts
+++ src/org/org.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrgService } from './org.service';
 import { OrgController } from './org.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [OrgController],
   providers: [OrgService],
 })
