71765e2b57776f78a075766fa85fe7b49757de6d
Kumar <kbishwa03kalyan@gmail.com>
2024-10-17 12:48:54 +0530

    added get all orgs endpoint

--- src/org/org.controller.ts
+++ src/org/org.controller.ts
@@ -1,9 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { OrgService } from './org.service';
+import { Organisation } from '@prisma/client';
 
 @Controller('org')
 export class OrgController {
   constructor(private readonly orgService: OrgService) {}
 
-  
+  @Get()
+  async getAllOrgs(@Param('id') id: string): Promise<Organisation[]> {
+    return this.orgService.getAllOrgs(id);
+  }
 }
--- src/org/org.service.ts
+++ src/org/org.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Organisation } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class OrgService {}
+export class OrgService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async getAllOrgs(id: string): Promise<Organisation[]> {
+    const orgs: Organisation[] = await this.prismaService.organisation.findMany(
+      {
+        where: {
+          ownerId: id,
+        },
+      },
+    );
+
+    return orgs;
+  }
+}
